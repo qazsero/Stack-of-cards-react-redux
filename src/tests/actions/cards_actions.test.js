681a6e2e9cc0333655c783d2cad9dd5740490a0d
Fabian Nitka <4090603+qazsero@users.noreply.github.com>
2018-04-09 16:34:13 +0100

    Fixed code consistency

--- src/tests/actions/cards_actions.test.js
+++ src/tests/actions/cards_actions.test.js
@@ -1,31 +1,26 @@
 import * as actions from '../../actions/cards_actions'
 import { cardsGenerator } from '../../utils/cards_generator'
-import {
-    LOAD_CARDS,
-    DRAW_CARD_FROM_DECK,
-    SHUFFLE_DECK, 
-    SORT_HAND, 
-} from '../../actions/types'
+import * as types from '../../actions/types'
 
 describe('actions', () => {
     it('creates an action to load cards', () => {
         const deck = cardsGenerator()
-        const expectedAction = {type: LOAD_CARDS, payload: {deck}}
+        const expectedAction = {type: types.LOAD_CARDS, payload: {deck}}
         expect(actions.loadCards()).toEqual(expectedAction)
     })
 
     it('creates an action to draw a card from the deck', () => {
-        const expectedAction = {type: DRAW_CARD_FROM_DECK}
+        const expectedAction = {type: types.DRAW_CARD_FROM_DECK}
         expect(actions.drawCardFromDeck()).toEqual(expectedAction)
     })
 
     it('creats en action to shuffle the deck', ()=>{
-        const expectedAction = { type: SHUFFLE_DECK}
+        const expectedAction = { type: types.SHUFFLE_DECK}
         expect(actions.shuffleDeck()).toEqual(expectedAction)
     })
 
     it('creats en action to sort the hand', ()=>{
-        const expectedAction = { type: SORT_HAND}
+        const expectedAction = { type: types.SORT_HAND}
         expect(actions.sortHand()).toEqual(expectedAction)
     })
 })
